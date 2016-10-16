22b3050db41b056c55b8a7c20e67b5df2cb34dc2
Joseph Luck <joseph.luck@sky.com>
2016-10-16 16:29:27 +0100

    Removing console logs from company socket filter

--- src/filters/company.js
+++ src/filters/company.js
@@ -1,8 +1,6 @@
 module.exports = function(data, connection, hook) {
   if (data.company_id !== hook.params.user.company_id) {
-    console.log('Resource created but not for same company', data, connection.user)
     return false;
   }
-  console.log('Resource created for same company')
   return data;
 }
