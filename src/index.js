80f83887b717c14a5044d08f0a706e8fcf0a2490
Joseph Luck <joseph.luck@sky.com>
2016-11-06 13:55:52 +0000

    Setting up server config for admin and api apps

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 'use strict';
 
-const app = require('./app');
-const port = app.get('port');
-const server = app.listen(port);
+const feathers = require('feathers')
+const cors = require('cors');
 
+const mainApp = feathers()
+const feathersApp = require('./app');
+const apiApp = feathers()
+
+mainApp.options('*', cors())
+mainApp.use('/admin', feathersApp)
+mainApp.use('/api', apiApp);
+
+feathersApp.service('projects').on('updated', (project) => {
+  console.log('Updated project');
+});
+
+const port = feathersApp.get('port');
+const server = mainApp.listen(port);
 server.on('listening', () =>
-  console.log(`Feathers application started on ${app.get('host')}:${port}`)
+  console.log(`Feathers app started on ${feathersApp.get('host')}:${port}`)
 );
