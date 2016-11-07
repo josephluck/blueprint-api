c63fbac700bd7192df991c043e97388d8435a6be
Joseph Luck <joseph.luck@sky.com>
2016-11-07 14:56:47 +0000

    Adding JSON server config which serves the client-facing fake API

--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
 'use strict';
+const DynamicMiddleware = require('dynamic-middleware')
 
 const feathers = require('feathers')
+const jsonServer = require('json-server')
+const bodyParser = require('body-parser')
 const cors = require('cors');
 
 const mainApp = feathers()
 const feathersApp = require('./app');
-const apiApp = feathers()
+
+const apiApp = jsonServer.create();
+const apiRouter = DynamicMiddleware.create(jsonServer.router({
+  "posts": []
+}));
+apiApp.use(bodyParser.json());
+apiApp.use(jsonServer.defaults());
+apiApp.use(apiRouter.handler());
 
 mainApp.options('*', cors())
 mainApp.use('/admin', feathersApp)
 mainApp.use('/api', apiApp);
 
-feathersApp.service('projects').on('updated', (project) => {
-  console.log('Updated project');
+feathersApp.service('projects').find().then((projects) => {
+  let jsonData = {
+    "posts": [
+      {
+        "id": 1,
+        "title": "json-server",
+        "author": "typicode"
+      }
+    ],
+    "comments": [
+      {
+        "id": 1,
+        "comment": "Hey"
+      }
+    ]
+  };
+  apiRouter.replace(jsonServer.router(jsonData));
 });
+
+// feathersApp.service('projects').on('updated', (project) => {
+//   console.log('Updated project');
+// });
 
 const port = feathersApp.get('port');
 const server = mainApp.listen(port);
