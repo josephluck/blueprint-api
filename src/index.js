467ee0d9b733d91147b38c06dfa022f0c623f9f6
Joseph Luck <joseph.luck@sky.com>
2016-11-11 10:16:19 +0000

    Automatically updating API when projects change

--- src/index.js
+++ src/index.js
@@ -1,33 +1,61 @@
-'use strict';
+'use strict'
 const DynamicMiddleware = require('dynamic-middleware')
-const ApiDatabaseHelper = require('./utils/apiDatabaseHelper');
+const ApiDatabaseHelper = require('./utils/apiDatabaseHelper')
+const Feathers = require('feathers')
+const Socketio = require('feathers-socketio')
+const BodyParser = require('body-parser')
+const JsonServer = require('json-server')
 
-const feathers = require('feathers')
-const jsonServer = require('json-server')
-const bodyParser = require('body-parser')
-const cors = require('cors');
+const app = Feathers()
 
-const mainApp = feathers()
-const feathersApp = require('./app');
+app.configure(Socketio())
 
-mainApp.options('*', cors())
-mainApp.use('/admin', feathersApp)
+const projectJsonServers = {}
 
-feathersApp.service('projects').find().then((projects) => {
-  projects.data.forEach((project) => {
-    const projectDatabaseData = ApiDatabaseHelper.generateProjectDb(project.resources);
-    console.log(projectDatabaseData);
-    const projectJsonServer = jsonServer.create();
-    const projectRouter = jsonServer.router(projectDatabaseData);
-    projectJsonServer.use(bodyParser.json());
-    projectJsonServer.use(jsonServer.defaults());
-    projectJsonServer.use(projectRouter);
-    mainApp.use(`/api/${project._id}`, projectJsonServer);
-  })
-});
+const adminApp = require('./app')
 
-const port = feathersApp.get('port');
-const server = mainApp.listen(port);
+const setupProjectApi = (project) => {
+  console.log('----------------------------------------------')
+  let existingProjectMiddleware = projectJsonServers[project._id]
+
+  let projectDatabaseData = ApiDatabaseHelper.generateProjectDb(project.resources)
+  let projectJsonServer = JsonServer.create()
+  let projectRouter = JsonServer.router(projectDatabaseData)
+  projectJsonServer.use(BodyParser.json())
+  projectJsonServer.use(JsonServer.defaults())
+  projectJsonServer.use(projectRouter)
+
+  if (existingProjectMiddleware) {
+    let newProjectMiddleware = existingProjectMiddleware.replace(projectJsonServer)
+    existingProjectMiddleware = newProjectMiddleware
+    console.log('Updating project', project._id, projectDatabaseData)
+  } else {
+    let newProjectMiddleware = DynamicMiddleware.create(projectJsonServer)
+    projectJsonServers[project._id] = newProjectMiddleware
+    app.use(`/api/${project._id}`, newProjectMiddleware.handler())
+    console.log('Creating project', project._id, projectDatabaseData)
+  }
+  console.log('----------------------------------------------')
+}
+
+// Set up projects apis initially
+adminApp.service('projects').find().then((projects) => {
+  projects.data.forEach(setupProjectApi)
+  console.log('Finished setting up projects')
+  console.log('----------------------------------------------')
+})
+
+// Update project api when changed
+adminApp.service('projects').on('updated', (project) => {
+  console.log('Should update', project._id)
+  setupProjectApi(project)
+})
+
+app.use('/admin', adminApp)
+
+const port = adminApp.get('port')
+const server = app.listen(port)
+adminApp.setup(server)
 server.on('listening', () =>
-  console.log(`Feathers app started on ${feathersApp.get('host')}:${port}`)
-);
+  console.log(`Feathers app started on ${adminApp.get('host')}:${port}`)
+)
